4123149dcff9c26aef05d2e39d82e35f6e8f4d49
Therese Weidenstedt <kw222mi@student.lnu.se>
2024-02-07 17:03:37 +0100

    render 6 characters in list

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
-
-
 const getCharacter = async () => {
+  try {
     const response = await fetch("https://swapi.dev/api/people/");
-    const characters = await response.json()
-    console.log(characters)
-    renderCharacters(characters)
-}
+    const characters = await response.json();
+    console.log(characters);
+    displayCharactersResult(characters);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
+const displayCharactersResult = async (characters) => {
+  const numberOfCharacters = characters.results.length;
+  const itemsPerPage = 6;
+  const currentPage = 1;
+  const charDiv = document.getElementById("character-container");
 
-const renderCharacters = async (characters) => {
+  const renderCharacters = () => {
     console.log(characters.results);
-    const charDiv = document.getElementById("character-container");
-    let element = document.createElement('p')
-    charDiv.appendChild(element)
-    element.innerText= characters.results[0].name
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const slicedCharacterArray = characters.results.slice(start, end);
 
-}
+    let html = "<ul>";
+    slicedCharacterArray.forEach((char) => {
+      html += `<li><a href="#" class="char-link" data-id="${char.id}">${char.name}</a></li>`;
+    });
+    html += "</ul>";
 
-getCharacter()
+    // Add pagination controls
+    const paginationHTML = `
+            <div class="pagination">
+
+                <button class="prev-btn"${
+                  currentPage === 1 ? "disabled" : ""
+                }>Prev</button>
+                <span class="page-info">${start + 1}-${Math.min(
+      end,
+      numberOfCharacters
+    )} / ${numberOfCharacters}</span>
+                <button class="next-btn" ${
+                  end >= numberOfCharacters ? "disabled" : ""
+                }>Next</button>
+
+            </div>
+         `;
+
+    charDiv.innerHTML = html + paginationHTML;
+  };
+
+  renderCharacters();
+};
+
+getCharacter();
