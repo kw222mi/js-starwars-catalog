0d47060543ca06a3d6423ab534e63cdc7f5eca01
Therese Weidenstedt <kw222mi@student.lnu.se>
2024-02-09 10:04:17 +0100

    fix pagination and get character  details

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-const getCharacter = async () => {
+let charactersArr = [];
+let charactersObject;
+let numberOfCharacters;
+const itemsPerPage = 6;
+let currentPage = 1;
+const charDiv = document.getElementById("character-container");
+
+const getCharacter = async (url) => {
   try {
-    const response = await fetch("https://swapi.dev/api/people/");
-    const characters = await response.json();
-    console.log(characters);
-    displayCharactersResult(characters);
+    const response = await fetch(url);
+    charactersObject = await response.json();
+    console.log(charactersObject);
+    //console.log(charactersObject.results);
+    //console.log(charactersObject.results[0]);
+
+    return charactersObject;
   } catch (error) {
     console.error(error);
   }
 };
 
-const displayCharactersResult = async (characters) => {
-  const numberOfCharacters = characters.results.length;
-  const itemsPerPage = 6;
-  const currentPage = 1;
-  const charDiv = document.getElementById("character-container");
+const displayCharactersResult = async (charactersObject) => {
+  numberOfCharacters = charactersObject.count;
 
   const renderCharacters = () => {
-    console.log(characters.results);
     const start = (currentPage - 1) * itemsPerPage;
     const end = start + itemsPerPage;
-    const slicedCharacterArray = characters.results.slice(start, end);
+    const slicedCharacterArray = charactersArr.slice(start, end);
 
-    let html = "<ul>";
-    slicedCharacterArray.forEach((char) => {
-      html += `<li><a href="#" class="char-link" data-id="${char.id}">${char.name}</a></li>`;
+    let html = "<ul class='char-ul'>";
+    slicedCharacterArray.map((char, index) => {
+      html += `<li><a href="#" class="char-link" data-index="${index}">${char.name}</a></li>`;
     });
     html += "</ul>";
 
@@ -46,9 +52,69 @@ const displayCharactersResult = async (characters) => {
          `;
 
     charDiv.innerHTML = html + paginationHTML;
+
+    const prevBtn = document.querySelector(".prev-btn");
+    const nextBtn = document.querySelector(".next-btn");
+    const charList = document.querySelector(".char-ul");
+
+    charList.addEventListener("click", (e) => {
+      let index = e.target.attributes[2].value;
+      console.log(index);
+      renderDetails(slicedCharacterArray[index]);
+    });
+
+    prevBtn.addEventListener("click", () => {
+      if (currentPage > 1) {
+        currentPage--;
+        renderCharacters();
+      }
+    });
+
+    nextBtn.addEventListener("click", async () => {
+      if (end < numberOfCharacters) {
+        currentPage++;
+        console.log(end);
+        if (end + 6 > charactersObject.results.length) {
+          let nextCharacters = await getCharacter(charactersObject.next);
+          charactersObject = nextCharacters;
+          charactersArr = charactersArr.concat(nextCharacters.results);
+        }
+        renderCharacters();
+      }
+    });
   };
 
   renderCharacters();
 };
 
-getCharacter();
+const renderDetails = async (char) => {
+  let charDetails = document.querySelector(".char-info");
+  console.log(char.name);
+
+  const detailsHTML = `
+  <div>
+    <p><span>Name: </span>${char.name}</p>
+    <p><span>Height: </span>${char.height}</p>
+    <p><span>Mass: </span>${char.mass}</p>
+    <p><span>Hair color: </span>${char.hair_color}</p>
+    <p><span>Skin color: </span>${char.skin_color}</p>
+    <p><span>Eye color: </span>${char.eye_color}</p>
+    <p><span>Birth Year: </span>${char.birth_year}</p>
+    <p><span>Gender: </span>${char.gender}</p>
+
+  </div>
+  `;
+  
+  charDetails.innerHTML = detailsHTML;
+
+  let planetDetails = document.querySelector(".planet-info");
+  let planet = await getCharacter(char.homeworld);
+ 
+  const planetHTML = `
+    
+  `
+};
+
+charactersObject = await getCharacter("https://swapi.dev/api/people/");
+charactersArr = charactersObject.results;
+displayCharactersResult(charactersObject);
